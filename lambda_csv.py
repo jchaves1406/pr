35ba61e1d590bf11e7d94547e8f258ecdce509b4
Jesús Chaves Acero <84520072+jchaves1406@users.noreply.github.com>
2023-03-08 14:48:48 -0500

    Update lambda_csv.py

--- lambda_csv.py
+++ lambda_csv.py
@@ -6,13 +6,17 @@ import datetime
 s3 = boto3.client('s3')
 
 
-def save_csv():
+def lambda_handler(event, context):
     file_name_html = datetime.datetime.now().strftime('%Y-%m-%d') + '.html'
 
-    bucket_html = 'landing-casas-xxx'
-    s3.download_file(bucket_html, file_name_html, file_name_html)
+    # bucket_html = 'landing-casas-xxx'
+    # s3.download_file(bucket_html, file_name_html, file_name_html)
 
-    page = leer_pagina("/home/runner/work/pr/pr/"+file_name_html)
+    # guarda el contenido del archivo en una variable
+    response = s3.get_object(Bucket=bucket_name, Key=file_name_html)
+    file_content = response['Body'].read().decode('utf-8')
+
+    page = leer_pagina(file_content)
     bloques = obtener_bloques_informacion(page)
     atributos_casas = extraer_atributos_casa(bloques)
     df_casas = crear_dataframe_casas(atributos_casas)
@@ -28,7 +32,7 @@ def save_csv():
         'body': json.dumps(file_name_csv + " guardado.")
     }
 
-save_csv()
+# save_csv()
 
 # zappa deploy dev
 # test: zappa invoke apps.f
