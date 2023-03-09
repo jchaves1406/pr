8cecd6e6df93c417221255cf4d4e0523300c00dc
Jesús Chaves Acero <84520072+jchaves1406@users.noreply.github.com>
2023-03-08 23:54:53 -0500

    Update lambda_html.py

--- lambda_html.py
+++ lambda_html.py
@@ -4,7 +4,6 @@ import datetime
 # import xvfbwrapper
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from pyvirtualdisplay import Display
 import datetime
 import os
 
@@ -28,10 +27,7 @@ def lambda_handler(event, context):
     }
 
 def descargar_pagina(url):
-    display = Display(visible=0, size=(800, 600))
-    display.extra_display_args = ['+extension', 'RANDR', '+render', '-noreset']
-    display.xvfb_bin = '/usr/bin/Xvfb'  # especifica la ruta de XVFB aquí
-    display.start()
+
     # Ruta del driver en el archivo yml
     ubicacion = "/home/runner/work/pr/pr/chromedriver"
 
@@ -45,7 +41,6 @@ def descargar_pagina(url):
     driver = webdriver.Chrome(service=servicio, options=options)
 
     # especificar la ruta de XVFB en la variable de entorno DISPLAY
-    os.environ['DISPLAY'] = ':99'
 
     driver.get(url)
 
@@ -56,7 +51,6 @@ def descargar_pagina(url):
         f.write(driver.page_source)
 
     driver.quit()
-    display.stop()
 
 # zappa deploy dev
 # test: zappa invoke apps.f
